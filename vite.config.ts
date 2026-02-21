d3e9afdb18745bcaf56dd2aca09e3a03d399ff65
Nikit phadke <nikitph@gmail.com>
2026-02-21 10:41:20 +0530

    more changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-      formats: ['es'],
-      fileName: 'index',
-    },
-    rollupOptions: {
-      external: [
-        'react',
-        'react-dom',
-        'react/jsx-runtime',
-        'motion',
-        'motion/react',
-        'tailwindcss',
-      ],
-      output: {
-        preserveModules: true,
-        preserveModulesRoot: 'src',
-        entryFileNames: '[name].js',
-      },
-    },
-    sourcemap: true,
-    minify: false,
-  },
+  base: '/flux-ui/',
 })
